984c8833885c3bd1012fb81ffbebc01b2b0ade8f
Marcelo Klinger <marcelo.klinger@hotmail.com>
2018-10-15 22:02:00 -0300

    atualização

--- App.js
+++ App.js
@@ -4,11 +4,29 @@ import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.body}>
-        <ImageBackground source={require('./images/waterbg.png')} style={styles.bgimage}>
+        <View style={styles.body}>
+            <ImageBackground source={require('./images/waterbg.png')} style={styles.bgimage}>
+                    <View style={styles.infoArea}>
+                        <View style={styles.area}>
+                            <Text style={styles.areaTitulo}>Meta</Text>
+                            <Text style={styles.areaDado}>2000ml</Text>
+                        </View>
 
-        </ImageBackground>
-      </View>
+                        <View style={styles.area}>
+                            <Text style={styles.areaTitulo}>Consumido</Text>
+                            <Text style={styles.areaDado}>500ml</Text>
+                        </View>
+
+                        <View style={styles.area}>
+                            <Text style={styles.areaTitulo}>Status</Text>
+                            <Text style={styles.areaDado}>Horrivel</Text>
+                        </View>
+                </View>
+                <View styles={styles.pctArea}>
+                    <Text style={styles.pctText}>25%</Text>
+                </View>
+            </ImageBackground>
+        </View>
     );
   }
 }
@@ -16,10 +34,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     body: {
       flex: 1,
-        paddingTop: 20
+      paddingTop: 20
     },
     bgimage: {
       flex: 1,
-        width: null
+      width: null
+    },
+    infoArea: {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: 70
+    },
+    area: {
+      flex: 1,
+      alignItems: 'center'
+    },
+    areaTitulo: {
+      color: '#45b2fc'
+    },
+    areaDado: {
+        color: '#2b4274',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    pctArea: {
+        marginTop: 100,
+        alignItems: 'center'
+    },
+    pctText: {
+        fontSize: 70,
+        color: '#FFFFFF',
+        backgroundColor: 'transparent'
     }
 });
